d322a0b2cf4cdc686ede8ed9cfd154ea81433425
root <root@localhost.localdomain>
2009-09-16 06:11:51 +1000

    Adding admin interface

--- listeners/http.py
+++ listeners/http.py
@@ -18,7 +18,32 @@
 
 from twisted.web import server, resource
 
-class APNSResource(resource.Resource):
+class APNSRootResource(resource.Resource):
+    """
+    The root apns resource.
+    """
+    def __init__(self, daemon, **kwds):
+        resource.Resource.__init__(self)
+        self.apns_daemon = daemon
+
+class APNSAdminResource(resource.Resource):
+    """
+    The admin resource handler.
+    Requests to /admin/ will usually be made from the webserver that is
+    acting as the frontend for the admin (eg gae).  The admin does things
+    like manage app specific passwords and the app's provisioning
+    certificates.
+    """
+    isLeaf = True
+    def __init__(self, daemon, **kwds):
+        resource.Resource.__init__(self)
+        self.apns_daemon = daemon
+
+class APNSAppResource(resource.Resource):
+    """
+    A resource which handles all calls to /apps/.
+    Requests to /apps/.../ are treated as messages to be forwarded to APNS.
+    """
     isLeaf = True
     def __init__(self, daemon, **kwds):
         resource.Resource.__init__(self)
@@ -50,9 +75,16 @@ class APNSResource(resource.Resource):
 
 class APNSSite(server.Site):
     def __init__(self, daemon, **kwds):
-        self.root_resource  = APNSResource(daemon, **kwds)
         self.apns_daemon    = daemon
+        self.root_resource  = APNSRootResource(daemon, **kwds)
+        self.admin_resource = APNSAdminResource(daemon, **kwds)
+        self.apps_resource  = APNSAppsResource(daemon, **kwds)
+        self.root_resource.putChild("admin", self.admin_resource)
+        self.root_resource.putChild("apps", self.apps_resource)
 
         server.Site.__init__(self, self.root_resource)
+
+        # check other things like whether we want to do SSL 
+        # and which host/port we want to listen and so on...
         daemon.reactor.listenTCP(kwds['port'], self)
 

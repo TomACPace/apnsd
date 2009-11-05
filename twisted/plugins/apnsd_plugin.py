25901b919dfcb9cfc6703d75f2a5ee34c42d0e13
sri <sri@begada>
2009-11-06 06:48:26 +1100

    Fixed syntax errors

--- twisted/plugins/apnsd_plugin.py
+++ twisted/plugins/apnsd_plugin.py
@@ -4,8 +4,8 @@ from twisted.application.service import ServiceMaker
 APNSD_Plugin = ServiceMaker(
     "APNS Daemon",
     "apnsd.tap",
-    ("A daemon for relaying reqeusts to the Apple Push Notification Server
-    in a simple and easy way."),
+    ("A daemon for relaying reqeusts to the Apple Push Notification Server",
+    "in a simple and easy way."),
     "apnsd"
 )
-_
+

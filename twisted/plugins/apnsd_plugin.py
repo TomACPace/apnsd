4b0c4f7275ca2148206d319c71529f1b19a71a2b
Sri Panyam <sri.panyam@gmail.com>
2011-07-12 15:40:50 +1000

    Spelling fix

--- twisted/plugins/apnsd_plugin.py
+++ twisted/plugins/apnsd_plugin.py
@@ -4,7 +4,7 @@ from twisted.application.service import ServiceMaker
 APNSD_Plugin = ServiceMaker(
     "APNS Daemon",
     "apnsd.tap",
-    """A daemon for relaying reqeusts to the Apple Push Notification Server in a simple and easy way.""",
+    """A daemon for relaying requests to the Apple Push Notification Server in a simple and easy way.""",
     "apnsd"
 )
 
